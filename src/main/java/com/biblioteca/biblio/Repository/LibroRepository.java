5c7378f261d2abf3c7caae80d50a404cbe61db63
skt-t <co.leveke@duocuc.cl>
2025-06-20 22:47:53 -0400

    Agregue las query de busqueda de libro, aun falta agregar funcion de agregado de libros

---
+++ src/main/java/com/biblioteca/biblio/Repository/LibroRepository.java
@@ -0,0 +1,26 @@
+package com.biblioteca.biblio.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.biblioteca.biblio.Model.Libro;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+@Repository
+public interface LibroRepository extends JpaRepository<Libro, Integer> {
+    
+    // Encuentra por nombre
+    // Devuelve una lista de libros que coinciden con el nombre
+    @Query("SELECT l FROM Libro WHERE l.nombre LIKE %:nombre%")
+    List<Libro> findByNombreContaining(@Param("nombre") String nombre);
+
+    // Encuentra a todos los libros
+    // Devuelve una lista de todos los libros
+    @Query("SELECT l FROM Libro l")
+    List<Libro> findAllLibros();
+
+
+}
---
+++ src/main/java/com/biblioteca/biblio/Service/LibroService.java
@@ -0,0 +1,27 @@
+package com.biblioteca.biblio.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
+import com.biblioteca.biblio.Model.Libro;
+import com.biblioteca.biblio.Repository.LibroRepository;
+import java.util.List;
+
+@Service
+@Transactional
+public class LibroService {
+
+    @Autowired
+    private LibroRepository libroRepository;
+    
+    public List<Libro> findByNombreContaining(String nombre) {
+        return libroRepository.findByNombreContaining(nombre);
+    }
+
+    public List<Libro> findAllLibros() {
+        return libroRepository.findAll();
+    }
+
+
+
+}
